16207667302c2e1bb5d87ca3027815042ef18507
esteficodes <estefaniaquevedolusby@gmail.com>
2021-03-10 15:50:13 +0100

    added one component for the header

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import data from './data.json'
 import {Album} from './Album.js'
+import {Header} from './Header'
 
 console.log(data)
 
@@ -10,13 +11,17 @@ const albumArray = data.albums.items
 
 export const App = () => {
   return (
+  <> 
+    <Header />
     <div>
-      <h1>New albums & singles</h1>
       {albumArray.map((album) =>{
-        return ( 
+        return (  
           <Album key={album.name} name={album.name} artist={album.artists[0].name} picture={album.images[0].url} externalurls={album.artists[0].external_urls.spotify} albumurl={album.external_urls.spotify}/>
         )
       })}
     </div>
+  </>
   )
 }
+
+
---
+++ code/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = (props) => {
+    return (
+        <div>
+            <h1>New albums & singles</h1>
+        </div>
+    )
+}
