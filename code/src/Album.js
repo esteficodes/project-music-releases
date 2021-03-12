4f0df564580e9d417053fdf2a9849980c8cb86c5
esteficodes <estefaniaquevedolusby@gmail.com>
2021-03-12 11:28:23 +0100

    added color for icons

--- code/src/Album.js
+++ code/src/Album.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 export const Album = (props) => {
     return (
         <div className="card"> 
@@ -7,9 +8,9 @@ export const Album = (props) => {
             <a href={props.albumurl}>         
             <img className="image" src={props.picture} alt="album cover"/></a>
             <div className="icons">
-            <img className="heart-icon" src="/icons/heart.svg" alt="" />
-            <img className="play-icon"  src="/icons/play.svg" alt="" />
-            <img className="dots-icon"  src="/icons/dots.svg" alt="" />
+            <img className="heart-icon" src="icons/heart.svg" alt="" />
+            <img className="play-icon"  src="icons/play.svg" alt="" />
+            <img className="dots-icon"  src="icons/dots.svg" alt="" />
             </div>
         </div>
             <h2 className="albums-name">{props.name}</h2>
