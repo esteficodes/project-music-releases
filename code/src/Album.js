8aafda7280ea429bff5ac442d389e902637b81fb
esteficodes <estefaniaquevedolusby@gmail.com>
2021-03-12 09:46:39 +0100

    added icons, changed size for play icon on hover, added underline for names on hover

--- code/src/Album.js
+++ code/src/Album.js
@@ -7,14 +7,14 @@ export const Album = (props) => {
             <a href={props.albumurl}>         
             <img className="image" src={props.picture} alt="album cover"/></a>
             <div className="icons">
-            <img className="heart-icon" src="icons/heart.png" alt="" />
-            <img className="play-icon"  src="icons/play.png" alt="" />
-            <img className="dots-icon"  src="icons/dots.png" alt="" />
+            <img className="heart-icon" src="/icons/heart.svg" alt="" />
+            <img className="play-icon"  src="/icons/play.svg" alt="" />
+            <img className="dots-icon"  src="/icons/dots.svg" alt="" />
             </div>
         </div>
-            <h2>{props.name}</h2>
+            <h2 className="albums-name">{props.name}</h2>
             <a className="link" href={props.externalurls}>
-            <h3>{props.artist}</h3></a>           
+            <h3 className="artist-name">{props.artist}</h3></a>           
         </div>
 
     )
